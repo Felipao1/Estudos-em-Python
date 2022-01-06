f605a4aaca9da5e552048bee42c82500a5211dc4
Felipe Rodrigues Gimenes Cano <93270819+Felipao1@users.noreply.github.com>
2022-01-06 19:04:29 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,9 @@
+nome=input('Qual é o seu nome?')
+idade=int(input('Quantos anos você tem?'))
+if idade >= 18:{
+    print(nome,'você já é maior de idade')
+}
+else:{
+    print(nome,'você ainda é menor de idade')
+}
+
