ca3606a1947b90dec758852751365296cf96b2e5
Felipe Rodrigues Gimenes Cano <93270819+Felipao1@users.noreply.github.com>
2022-01-06 19:34:51 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,11 @@
+nome=input('Qual é o seu nome?')
+nota1=float(input('Nota primeiro período'))
+nota2=float(input('Nota segundo período'))
+notaFinal=(nota1+nota2)/2
+if (notaFinal)>=(6):
+    print(nome+' '+'foi aprovado')
+else:
+        print(nome+' '+'foi reprovado')
+   
+
+  
