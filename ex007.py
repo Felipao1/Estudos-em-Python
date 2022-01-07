06c1865599438969a3a87b4a09ac18c95ffc3263
Felipe Rodrigues Gimenes Cano <93270819+Felipao1@users.noreply.github.com>
2022-01-07 08:19:00 -0300

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,17 @@
+n1=float(input('digite um número '))
+n2=float(input('digite outro número '))
+print('A-ADIÇÃO\n','B-SUBTRAÇÃO\n','C-MULTIPLICAÇÃO\n','D-DIVISÃO\n','E-EXPONENCIAÇÃO')
+operacao=input('Escolha uma operação ')
+if operacao=='A':
+    print(n1+n2)
+if operacao=='B':
+    print(n1-n2)
+if operacao=='C':
+    print(n1*n2)
+if operacao=='D':
+    print(n1/n2)
+if operacao=='E':
+    print(n1**n2)
+
+
+
