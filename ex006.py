a402fb0dcc0d9109d6052e1b5984092ee0481f72
Felipe Rodrigues Gimenes Cano <93270819+Felipao1@users.noreply.github.com>
2023-06-08 12:28:11 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,6 @@
+n1=float(input('Digite um número:'))
+n2=float(n1*2)
+n3=float(n1*3)
+n4=float(n1**1/2)
+
+print('O dobro de {} é {}, seu triplo é {} e sua raíz quadrada é {}.' .format(n1, n2, n3, n4))
